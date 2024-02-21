40f56493e93cca3367dbf366b3f7a4149fc0e43b
Will <doggy.huang@gmail.com>
2024-02-21 12:14:11 +0800

    加入 HttpClient 的測試設定

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+
+var httpTestingController: HttpTestingController;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
     }).compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -17,6 +27,7 @@ describe('AppComponent', () => {
   it(`should have the 'demo1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('demo1');
   });
 
@@ -25,5 +36,27 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, demo1');
+  });
+
+
+  it(`should req'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockData = { data: 'test' };
+
+    app.getData().subscribe(data => {
+      expect(data).toEqual(mockData.data);
+    });
+
+    var req = httpTestingController.expectOne('http://localhost:4200/assets/a.json', 'Home page');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockData);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo1';
+  http = inject(HttpClient);
+
+  ngOnInit() {
+  }
+
+  getData() {
+    return this.http.get<any>('http://localhost:4200/assets/a.json')
+      .pipe(
+        map((data) => data.data)
+      );
+  }
 }
